38f7cadc0f704eb0384d1993da36740c87cdcd88
lwy123-wq <l2158223895@qq.com>
2022-01-23 10:44:49 +0800

    didi-new

---
+++ workunit/src/main/java/com/example/workunit/WorkunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.workunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkunitApplication.class, args);
+    }
+
+}
---
+++ workunit/src/test/java/com/example/workunit/WorkunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
